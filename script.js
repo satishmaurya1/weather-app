919095989753ce7d8a2e1a24ba6fffc9a0f7372c
Satish maurya <satishmaurya15216@gmail.com>
2020-12-14 12:19:48 +0530

    updated js and css

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const textarea=document.querySelector(".textarea");
 const btn=document.querySelector(".btn");
 btn.addEventListener("click",clickbtn)
+const img=document.querySelector(".content .img")
 const contentheading1=document.querySelector(".content .heading1")
 const contentheading2=document.querySelector(".content .heading2")
 const contentheading3=document.querySelector(".content .heading3")
 const contentheading4=document.querySelector(".content .heading4")
 const contentheading5=document.querySelector(".content .heading5")
+const contentheading6=document.querySelector(".content .heading6")
+const contentheading7=document.querySelector(".content .heading7")
 
 
 function error(){
     alert("oops something happened")
 }
+
+
+
 
 function clickbtn(){
 
@@ -18,11 +24,22 @@ fetch(`https://api.weatherapi.com/v1/current.json?key=6b92d33f29994aeebb32344620
 .then(reponse=>{ return reponse.json()})
 .then(data=>{
     console.log(data)
+    
+    // const img =document.createElement("img");
+    img.classList.add("imgtag")
+    console.log(img)
+    img.setAttribute("src"," ")
+    img.src=(data.current.condition.icon)
+    
+    // document.body.appendChild(img)
+
     contentheading1.innerText=data.location.name
     contentheading2.innerText=data.location.region
     contentheading3.innerText=data.location.country
     contentheading4.innerText=data.current.temp_c + " ℃"
     contentheading5.innerText=data.current.condition.text
+    contentheading6.innerText="Humidity "+data.current.humidity
+    contentheading7.innerText="wind_kph "+data.current.wind_kph
 })
 .catch(error)
 
